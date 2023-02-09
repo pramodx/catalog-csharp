7820fa880666d2fcee7b00d8d36b3d3e38c1d374
Pramod Nair <pramodx@gmail.com>
2023-02-10 09:18:27 +1100

    first commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,13 @@
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+    }
+}
